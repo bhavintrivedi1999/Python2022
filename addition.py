dea4e537073c7023ca0fb2a28b7583a1842f0ca6
BHAVIN <trivedibhavin1999@gmail.com>
2022-02-05 13:30:59 +0530

    cast innput to integer

--- addition.py
+++ addition.py
@@ -1,5 +1,5 @@
 #addithio of two numbets
-n1= input("enter first number")
-n2= input("enter second number")
+n1= int(input("enter first number:-"))
+n2= int(input("enter second number:-"))
 sum=n1 + n2
 print(sum)
